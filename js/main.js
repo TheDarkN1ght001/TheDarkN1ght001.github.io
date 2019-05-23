540e64deabd21013869872a9871899f53451479b
TheDarkN1ght001 <1132962475@qq.com>
2019-05-23 20:51:29 +0800

    修改后的内容

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["花姐", "前端", "后端", "全栈.", "爱媳妇儿"],
+      strings: ["伟哥哥", "前端", "后端", "全栈.", "爱媳妇儿"],
       typeSpeed: 100,
       loop: true,
     });
